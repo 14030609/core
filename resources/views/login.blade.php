a9fe658c3c82f56ce8990bff827c3866754f9482
Mike <mike.angyz@gmail.com>
2021-12-13 12:25:43 -0600

    x3

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- Bootstrap 4 -->
     <link rel='stylesheet' href='login/css/main.css' type='text/css' media='all' />
     <link rel='stylesheet' href='login/css/util.css' type='text/css' media='all' />
